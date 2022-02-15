ebb082170e0537d22defa9851fc58c9655c14085
anil <31825249+anilkumar23@users.noreply.github.com>
2022-02-15 10:38:02 +0530

    Update Address.java

--- src/main/java/com/org/employee/domain/Address.java
+++ src/main/java/com/org/employee/domain/Address.java
@@ -4,9 +4,6 @@ import java.io.Serializable;
 
 public class Address implements Serializable{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -1772727578224763785L;
 	
 	private int addressId;
